b4414dc026e8aea07487c7bd7fd9036f2cb7a943
Jason <jasonste123@gmail.com>
2021-01-07 22:13:35 -0500

    Starting the file

---
+++ main.py
@@ -0,0 +1,5 @@
+#This is a place for me to begin trying to understand neural networks and deep learning.. 
+#This will be done through the use of youtube/google/stack overflow -- self taught
+
+
+print("test")
