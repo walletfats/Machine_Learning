b2fcb5d604ca3838b1ed01d0d470545a28418409
Jason <jasonste123@gmail.com>
2021-01-10 20:50:39 -0500

    Updated todo

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 #This is a place for me to begin trying to understand neural networks and deep learning.. 
 #This will be done through the use of youtube/google/stack overflow -- self taught
 
+# TODO
+# Pytorch or tensorflow
+# Learning Support Vector Machines 
+# Linear Regression review 
+# KNN algorithm
+# Applying all of these fundementals in code and understanding them well
+# Implement a neural network
 
-print("test")
+
+import pytorch as pt
+
+
